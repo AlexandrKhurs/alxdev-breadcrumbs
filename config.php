50ec52d00152f326f4d7868e731d015df3dfae00
Alexandr <alexandr.khurs@gmail.com>
2024-01-22 01:48:21 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,11 +8,13 @@ $this->exclude_categories = ['uncategorized'];
 $this->post_types_settings = [
     'post' => [
         'top_page_slug' => 'news',
-        'use_taxonomy' => 'category'
+        'use_taxonomy' => 'category',
+        // 'display-post-name' => false,
     ],
     'products' => [
         'top_page_slug' => 'products',
-        'use_taxonomy' => 'products-category'
+        'use_taxonomy' => 'products-category',
+        // 'display-post-name' => true, // tru by default
     ],
 ];
 $this->separator = ' / ';
